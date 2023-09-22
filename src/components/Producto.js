1e498e6454243a4cd7ac61afad56052f7098912f
Jordimajo <144838158+Jordimajo@users.noreply.github.com>
2023-09-22 16:16:00 +0200

    modal updated

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -6,20 +6,25 @@ const Producto = ({ producto }) => {
   const [descripcionDesplegada, setDescripcionDesplegada] = useState(false);
   const [modalAbierto, setModalAbierto] = useState(false);
 
+  const handleImageClick = () => {
+    setModalAbierto(true);
+  }
+
   return (
     <div className="producto">
-      <img
-        src={`/images/${producto.imagen}`}
-        alt={producto.nombre}
+      <img 
+        src={`/images/${producto.imagen}`} 
+        alt={producto.nombre} 
         className="producto-imagen"
-        onClick={() => setModalAbierto(true)}
+        onClick={handleImageClick}
+        onTouchStart={handleImageClick} 
       />
       <h2>{producto.nombre}</h2>
       <button onClick={() => setDescripcionDesplegada(!descripcionDesplegada)}>
         Descripción {descripcionDesplegada ? "-" : "+"}
       </button>
       {descripcionDesplegada && <p className="producto-descripcion">{producto.descripcion}</p>}
-      {modalAbierto && (  // Renderizar el modal si modalAbierto es true
+      {modalAbierto && (
         <Modal onClose={() => setModalAbierto(false)}>
           <img src={`/images/${producto.imagen}`} alt={producto.nombre} style={{ maxWidth: '90%', maxHeight: '80vh' }} />
         </Modal>
